fa38a690b9c00bb5a2db6d3834100f66506567b4
Oleg Bolshov <bolshovdev@yandex.ru>
2020-07-12 23:34:03 +0500

    add server config

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/leogip/golang-jwt-rest
 go 1.14
 
 require (
-	github.com/gorilla/handlers v1.4.2 // indirect
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.4
 	github.com/joho/godotenv v1.3.0
 	github.com/sirupsen/logrus v1.6.0
 )
--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+
+	"github.com/leogip/golang-jwt-rest/logger"
+)
+
+var (
+	log = logger.New("main")
 )
 
 
 func main() {
 	godotenv.Load()
+
+	routes := mux.NewRouter()
+
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	log.Info("Server Is Running At ", os.Getenv("PORT"))
+	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), handlers.CORS(headersOk, originsOk, methodsOk)(routes)))
 }
